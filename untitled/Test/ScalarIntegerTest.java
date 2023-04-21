7c41e64ed97a00180b57f8e7cc00ed53fad18f06
sriftin <saar.riftin@gmail.com>
2023-04-21 12:29:40 +0300

    scalarTEST

---
+++ untitled/Test/ScalarIntegerTest.java
@@ -0,0 +1,48 @@
+import static org.junit.Assert.*;
+
+public class ScalarIntegerTest {
+
+    @org.junit.Test
+    public void add() {
+    }
+
+    @org.junit.Test
+    public void testAdd() {
+    }
+
+    @org.junit.Test
+    public void mul() {
+    }
+
+    @org.junit.Test
+    public void testMul() {
+    }
+
+    @org.junit.Test
+    public void testMul1() {
+    }
+
+    @org.junit.Test
+    public void neg() {
+    }
+
+    @org.junit.Test
+    public void power() {
+    }
+
+    @org.junit.Test
+    public void sign() {
+    }
+
+    @org.junit.Test
+    public void testToString() {
+    }
+
+    @org.junit.Test
+    public void getNumber() {
+    }
+
+    @org.junit.Test
+    public void testEquals() {
+    }
+}
---
+++ untitled/Test/ScalarRationalTest.java
@@ -0,0 +1,50 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ScalarRationalTest {
+
+    @Test
+    public void add() {
+    }
+
+    @Test
+    public void testAdd() {
+    }
+
+    @Test
+    public void testAdd1() {
+    }
+
+    @Test
+    public void mul() {
+    }
+
+    @Test
+    public void testMul() {
+    }
+
+    @Test
+    public void testMul1() {
+    }
+
+    @Test
+    public void neg() {
+    }
+
+    @Test
+    public void power() {
+    }
+
+    @Test
+    public void sign() {
+    }
+
+    @Test
+    public void reduce() {
+    }
+
+    @Test
+    public void testEquals() {
+    }
+}
