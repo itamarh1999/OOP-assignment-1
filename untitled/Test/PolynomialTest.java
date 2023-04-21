cd3a926b3f35dc268b41954bbebe026495bd64ef
sriftin <saar.riftin@gmail.com>
2023-04-21 16:51:42 +0300

    poly test

---
+++ untitled/Test/PolynomialTest.java
@@ -0,0 +1,34 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PolynomialTest {
+
+    @Test
+    public void build() {
+    }
+
+    @Test
+    public void add() {
+    }
+
+    @Test
+    public void mul() {
+    }
+
+    @Test
+    public void evaluate() {
+    }
+
+    @Test
+    public void derivative() {
+    }
+
+    @Test
+    public void testToString() {
+    }
+
+    @Test
+    public void testEquals() {
+    }
+}
